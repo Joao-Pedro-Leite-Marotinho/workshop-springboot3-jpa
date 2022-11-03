5951cc6035c417535459cf34a86f01c72029f727
JP <jplmarotinho22@gmail.com>
2022-11-03 07:45:07 -0300

    User entity and resource

---
+++ src/main/java/com/JP/course/resources/UserResource.java
@@ -0,0 +1,26 @@
+package com.JP.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.JP.course.entities.User;
+
+//this annotation marks this class as a web resource that is implemented by a rest controller
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	//testing
+	//ResponseEntity is an specific type that respond to web requisitions
+	//it uses the path /users
+	
+	//testing
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "999999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
