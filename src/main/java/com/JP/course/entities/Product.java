a867fe9bca43e8fede3f2c5cad3554f7e613688d
JP <jplmarotinho22@gmail.com>
2022-11-18 09:54:52 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/JP/course/entities/Product.java
+++ src/main/java/com/JP/course/entities/Product.java
@@ -5,6 +5,8 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -33,10 +36,14 @@ public class Product implements Serializable {
 	// interface
 	// Association with other entities
 
+	//it's Set and not List because with Set we let JPA know that we won't allow the same item be repeated
 	@ManyToMany
 	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "categor_id"))
 	private Set<Category> categories = new HashSet<>();
-
+	
+	@OneToMany(mappedBy = "id.product")//id comes from OrderItemOK id
+	private Set<OrderItem> items = new HashSet<>();
+	
 	public Product() {
 	}
 
@@ -90,6 +97,15 @@ public class Product implements Serializable {
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for(OrderItem x : items) {
+			set.add(x.getOrder());
+		}
+		return set;
 	}
 
 	@Override
