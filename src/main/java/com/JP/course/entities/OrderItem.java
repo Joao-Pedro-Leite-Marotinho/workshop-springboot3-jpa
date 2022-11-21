832508ed042406f458f83d7013a96616b82a5762
JP <jplmarotinho22@gmail.com>
2022-11-21 07:14:18 -0300

    Subtotal and Total methods

--- src/main/java/com/JP/course/entities/OrderItem.java
+++ src/main/java/com/JP/course/entities/OrderItem.java
@@ -66,6 +66,10 @@ public class OrderItem implements Serializable{
 	public void setQuantity(Integer quantity) {
 		this.quantity = quantity;
 	}
+	
+	public double getSubTotal() {
+		return price * quantity;
+	}
 
 	@Override
 	public int hashCode() {
